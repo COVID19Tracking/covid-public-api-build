88707c00fdd0543ea205cfc7dd4181e205aca896
Kevin Miller <keveemiller@gmail.com>
2020-05-28 11:43:36 -0400

    chore: Add webhook step

--- src/output/git.js
+++ src/output/git.js
@@ -1,11 +1,23 @@
 const git = require('nodegit')
+const fetch = require('node-fetch')
 
 module.exports = () => {
-  git.Repository.open('./_api')
-    .then((repo) => {
-      return repo.getStatus()
+  if (typeof process.env.SITE_BUILD_WEBHOOK === 'undefined') {
+    return
+  }
+  const changedFiles = []
+  git.Repository.open('./_api').then((repo) => {
+    git.Status.foreach(repo, (file) => {
+      if (file.search('status') === -1) {
+        changedFiles.push(file)
+      }
     })
-    .then((status) => {
-      console.log(status.byIndex())
-    })
+    if (changedFiles.length) {
+      fetch(process.env.SITE_BUILD_WEBHOOK, {
+        method: 'post',
+        body: '{}',
+        headers: { 'Content-Type': 'application/json' },
+      })
+    }
+  })
 }
