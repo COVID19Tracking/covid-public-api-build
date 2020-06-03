2bcdcf4c05382f3aa5a50857b4946867c1eca471
Kevin Miller <keveemiller@gmail.com>
2020-06-03 08:07:08 -0400

    chore: Use new racial data tracker data

--- config/sources/race-separate.js
+++ config/sources/race-separate.js
@@ -3,7 +3,7 @@ module.exports = {
   path: 'race/states-separate.{format}',
   tags: ['Racial data tracker'],
   description: 'Racial data on states who separate race and ethnicity',
-  sheetId: '1FnrtpGkm9tuWg8FLavkIHhJSx0Z-X5QJUfbkPhgL3JQ',
+  sheetId: '1aPxG_isDWFMbxgnBqx5h8r04YkLn2lsaNiC5U9VfhOw',
   worksheetId: '1837531601',
   fieldDefinitions: [
     {
