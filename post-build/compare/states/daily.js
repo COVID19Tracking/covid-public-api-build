e85474784873b4fbc2149244fb8a333ca6f0e480
Kevin Miller <keveemiller@gmail.com>
2020-06-26 15:57:25 -0600

    chore: Add stricter testing for fields

--- post-build/compare/states/daily.js
+++ post-build/compare/states/daily.js
@@ -2,6 +2,24 @@ const fs = require('fs-extra')
 const fetch = require('node-fetch')
 const ignoredFields = ['notes']
 
+expect.extend({
+  toMatchField(field, v1, v2) {
+    if (v1[field] === v2[field]) {
+      return {
+        message: () =>
+          `${field} matches:: State: ${v2.state}, Date: ${v2.date}`,
+        pass: true,
+      }
+    } else {
+      return {
+        message: () =>
+          `${field} does not match:: State: ${v2.state}, Date: ${v2.date}, Current: ${v1[field]} Preview: ${v2[field]} `,
+        pass: false,
+      }
+    }
+  },
+})
+
 describe('States daily', () => {
   it('has the same fields', async () => {
     const v1 = await fetch(
@@ -9,10 +27,14 @@ describe('States daily', () => {
     ).then((response) => response.json())
     const v2 = fs.readJsonSync('./_api/v1/states/daily.json')
     expect(v2.length).toBeGreaterThan(v1.length - 100)
-    Object.keys(v1[0]).forEach((key) => {
-      if (ignoredFields.indexOf(key) === -1) {
-        expect(v2[0]).toHaveProperty(key)
-      }
+    v1.forEach((rowV1) => {
+      const rowV2 = v2.find(
+        (a) => a.date === rowV1.date && a.state === rowV1.state
+      )
+      expect(rowV2).toBeTruthy()
+      Object.keys(rowV1).forEach((field) => {
+        expect(field).toMatchField(rowV1, rowV2)
+      })
     })
   })
 })
