c300890ec0e7321d9f6e568a3ae6e0cc21ed50a6
Kevin Miller <keveemiller@gmail.com>
2020-08-31 09:57:30 -0700

    chore(test): Remove press post-build test

--- post-build/v1/press.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs-extra')
-const csv = require('csv-parser')
-const checkFile = require('../utilities/check-file')
-
-describe('File : Press', () => {
-  it('exists', () => {
-    checkFile('_api/v1/internal/press')
-  })
-})
