9c691115937a581bfc759fb2732e2951e36daff2
Kevin Miller <keveemiller@gmail.com>
2020-12-09 14:46:29 -0800

    chore: Add API v2 builder

--- src/run/index.js
+++ src/run/index.js
@@ -1,16 +1,23 @@
 const openApi = require('../output/openapi')
 const graphQl = require('../output/graphql')
 const config = require('../../config')
+const v2 = require('./v2')
 
 const api = openApi(config)
 const sdl = graphQl(config)
 module.exports = (sources, sourceComplete, done) => {
-  Promise.all(sources).then((response) => {
-    response.forEach((source) => {
-      sourceComplete(source)
-      api.addSource(source.source)
-      sdl.addDefinition(source.source)
+  Promise.all(sources)
+    .then((response) => {
+      response.forEach((source) => {
+        sourceComplete(source)
+        api.addSource(source.source)
+        sdl.addDefinition(source.source)
+      })
     })
-    done(api, sdl)
-  })
+    .then(() => {
+      return v2()
+    })
+    .then(() => {
+      done(api, sdl)
+    })
 }
