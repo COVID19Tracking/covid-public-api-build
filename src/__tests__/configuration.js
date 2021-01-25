be76a7f67cb57141e63e373d5caadb6e10bc1c3c
Kevin Miller <keveemiller@gmail.com>
2021-01-25 10:52:35 -0800

    chore: Bump yargs

--- src/__tests__/configuration.js
+++ src/__tests__/configuration.js
@@ -1,22 +1,7 @@
-const openApi = require('../output/openapi')
 const graphQl = require('../output/graphql')
-const oasNormalize = require('oas-normalize')
 const config = require('../../config')
 
 describe('Production configuration', () => {
-  it('generates a valid OpenAPI definition', (done) => {
-    const api = openApi(config)
-    Object.keys(config.sources).forEach((source) => {
-      api.addSource(config.sources[source])
-    })
-
-    const spec = new oasNormalize(api.getDefinition())
-    spec.validate((error) => {
-      expect(error).toBeNull()
-      done()
-    })
-  })
-
   it('generates a valid GraphQL schema', () => {
     const spec = graphQl(config)
     expect(() => {
--- src/__tests__/output/openapi.js
+++ src/__tests__/output/openapi.js
@@ -1,19 +1,8 @@
 const openApi = require('../../output/openapi')
 const config = require('../../__mocks__/config')
-const oasNormalize = require('oas-normalize')
 const sampleSource = require('../../__mocks__/output/schema')
 
 describe('OpenAPI builder', () => {
-  it('generates a valid OpenAPI spec preamble', (done) => {
-    const api = openApi(config)
-    expect(api.getDefinition().openapi).toBe('3.0.0')
-    const spec = new oasNormalize(api.getDefinition())
-    spec.validate((error) => {
-      expect(error).toBeNull()
-      done()
-    })
-  })
-
   it('processes a source path', () => {
     const api = openApi(config)
     const path = api.processSourcePath(sampleSource)
@@ -33,15 +22,5 @@ describe('OpenAPI builder', () => {
     api.addSource(sampleSource)
     const spec = api.getDefinition()
     expect(spec.paths['/test-api/v1/test2.{format}']).not.toBeUndefined()
-  })
-
-  it('generates a valid OpenAPI file', (done) => {
-    const api = openApi(config)
-    api.addSource(sampleSource)
-    const spec = new oasNormalize(api.getDefinition())
-    spec.validate((error) => {
-      expect(error).toBeNull()
-      done()
-    })
   })
 })
